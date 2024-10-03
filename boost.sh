a7bff8fd88332399a626e8b78e84bba1b4304e18
Vincent Chabannes <vincent.chabannes@feelpp.org>
2024-10-03 10:27:26 +0200

    add boost

---
+++ boost.sh
@@ -0,0 +1,30 @@
+BOOST_VERSION=1.86.0
+BOOST_VERSION_FILE=1_86_0
+BOOST_INSTALL_DIR=$1
+WORK_DIR=$2
+
+#source /opt/emsdk/emsdk_env.sh
+
+if [[ ! -d ${WORK_DIR} ]]; then
+    mkdir -p ${WORK_DIR}
+fi
+cd ${WORK_DIR}
+
+if [[ ! -f boost_${BOOST_VERSION_FILE}.tar.gz ]]; then
+    wget https://archives.boost.io/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION_FILE}.tar.gz | exit 1
+    tar zxf boost_${BOOST_VERSION_FILE}.tar.gz | exit 1
+fi
+
+cd boost_${BOOST_VERSION_FILE}
+
+./bootstrap.sh --with-icu=$EMSDK/upstream/emscripten/cache/ports/icu/icu
+printf "using clang : emscripten : emcc -s USE_ZLIB=1 -s USE_ICU=1 : <archiver>emar <ranlib>emranlib <linker>emlink <cxxflags>\"-std=c++17 -fPIC -s USE_ICU=1\" ;" | tee -a ./project-config.jam >/dev/null
+
+./b2 \
+    -q \
+    link=static \
+    toolset=clang-emscripten \
+    variant=release \
+    threading=single \
+    --with-program_options \
+    install --prefix=${BOOST_INSTALL_DIR}
